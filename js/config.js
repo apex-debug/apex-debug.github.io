e9e403ef1f28093742d60ed653fd34dac8fd17a7
apex-debug <61247986+apex-debug@users.noreply.github.com>
2020-02-23 11:14:04 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -72,7 +72,6 @@ var l_bgImageMapBased = false;
 // (Place the files in the backgrounds/images folder)
 // (You can also use external URL)
 var l_bgImages = [
-	"recruit.jpg",
 	"212.jpg",
 	"parade.jpg",
 	"fire.jpg",
