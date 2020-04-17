e25e2b668fba4ddacf310d054fae31da33e64dc6
apex-debug <61247986+apex-debug@users.noreply.github.com>
2020-04-17 14:50:18 +0100

    a

--- js/config.js
+++ js/config.js
@@ -114,7 +114,7 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{ogg: "brother.ogg", name: "Brother"},
+	{ogg: "brother", name: "Brother"},
 	{ogg: "", name: ""},
 	{youtube: "", name: ""},
 	{youtube: "", name: ""},
